30302edb5cd1b83d58ce46aaeda0c4a5b987c69d
AngelaRaysa <132140577+AngelaRaysa@users.noreply.github.com>
2023-08-25 09:38:39 -0500

    Create 1Car.py
    Sample Python code to calculate speed

---
+++ 1Car.py
@@ -0,0 +1,29 @@
+class Car:
+    def __init__(self, year_name, make):
+        self.__year_name = year_name
+        self.__make = make
+        self.__speed = 0
+     
+    def accelerate(self):
+        self.__speed = self.__speed + 5
+     
+    def brake(self):
+        self.__speed = self.__speed - 5
+    
+    def get_speed(self):
+        return self.__speed
+
+def main():
+    car = Car("2017", "Beamer")
+    
+    for i in range(5):
+        car.accelerate()
+        speed = car.get_speed()
+        print("Current Speed:", speed)
+    
+    for i in range(5):
+        car.brake()
+        speed = car.get_speed()
+        print("Current speed after brake:", speed)
+
+main()
